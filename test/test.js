2e1226332f4f8e2a885803977654f0bc79bb47c1
Charles L <charles.lobo@gmail.com>
2021-09-04 19:01:05 +1000

    feat: add more tests

--- test/test.js
+++ test/test.js
@@ -5,8 +5,14 @@ const jt = require('../')
 
 describe("jt", () => {
   describe("jt", () => {
+
     it("should ignore nulls", () => {
       assert.equal(jt(), null)
     })
+
+    it("should fail if doesn't have enough parameters", () => {
+      assert.equal(jt({},{}), null)
+    })
+
   })
 })
