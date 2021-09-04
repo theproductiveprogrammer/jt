5340b23ddab6dcaee1d3e66dfd77f41b9a06e1fd
Charles L <charles.lobo@gmail.com>
2021-09-04 18:58:43 +1000

    feat: add first test

---
+++ test/test.js
@@ -0,0 +1,10 @@
+'use strict'
+const assert = require('assert')
+
+describe("jt", () => {
+  describe("jt", () => {
+    it("should ignore nulls", () => {
+      assert.equal(jt(), null)
+    })
+  })
+})
