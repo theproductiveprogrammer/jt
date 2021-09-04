c0934edb62aeaf7d1f8c0833fca9cc1a58d7c18a
Charles L <charles.lobo@gmail.com>
2021-09-04 20:20:21 +1000

    feat: add tests

--- test/test.js
+++ test/test.js
@@ -44,5 +44,40 @@ describe("jt", () => {
       })
     })
 
+    it("should validate the empty payload", done => {
+      jt.check(token1, (err, header, payload) => {
+        assert.equal(err, null)
+        assert.deepEqual(header, header1)
+        assert.deepEqual(payload, payload1_empty)
+        done()
+      })
+    })
+
+
+    const header2 = {
+        iss: "my new service",
+        sub: "my good subjects",
+        exp: 2261470482317,
+        aud: "my kingdom for a good audience!",
+    }
+    const payload2 = {
+      data: "data 123",
+      and: "more data",
+      yet: "more and more and more",
+      you: "guessed it! - data!"
+    }
+    const secret2 = "ksauowiehjsdaiur205823075pjda;lfkjs;e9487qjadlfkjas;83q4-0rufjslkdvjd039485j;dklsjqp958aja90438rjfa"
+    const token2 = "eyJpc3MiOiJteSBuZXcgc2VydmljZSIsInN1YiI6Im15IGdvb2Qgc3ViamVjdHMiLCJleHAiOjIyNjE0NzA0ODIzMTcsImF1ZCI6Im15IGtpbmdkb20gZm9yIGEgZ29vZCBhdWRpZW5jZSEifQ.eyJkYXRhIjoiZGF0YSAxMjMiLCJhbmQiOiJtb3JlIGRhdGEiLCJ5ZXQiOiJtb3JlIGFuZCBtb3JlIGFuZCBtb3JlIiwieW91IjoiZ3Vlc3NlZCBpdCEgLSBkYXRhISJ9.iDp8CiBMhAY7swCXbxqUavfsWE1ohmXwqARejRV3vzc"
+
+    it("should return a valid token with given payload", () => assert.equal(jt(header2,payload2,secret2), token2))
+
+    it("should return the given payload", done => {
+      jt.decode(token2, (err, header, payload) => {
+        assert.equal(err, null)
+        assert.deepEqual(header, header2)
+        assert.deepEqual(payload, payload2)
+        done()
+      })
+    })
   })
 })
