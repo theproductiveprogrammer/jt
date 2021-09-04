0947165561be9c16c47fa7b92bc6fe654bb2004e
Charles L <charles.lobo@gmail.com>
2021-09-04 18:59:53 +1000

    feat: add skeleton framework

---
+++ index.js
@@ -0,0 +1,6 @@
+'use strict'
+
+function jt() {
+}
+
+module.exports = jt
--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
 'use strict'
 const assert = require('assert')
 
+const jt = require('../')
+
 describe("jt", () => {
   describe("jt", () => {
     it("should ignore nulls", () => {
