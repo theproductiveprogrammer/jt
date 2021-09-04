f0e4fc8f453d7ef1b7249fc3c6beb5ec8b5e86c4
Charles L <charles.lobo@gmail.com>
2021-09-04 19:50:34 +1000

    feat: support encoding

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 'use strict'
+const crypto = require('crypto')
 
-function jt() {
+function base64UrlEncode(str) {
+  return urlEncodeBase64(Buffer.from(str, 'utf8').toString('base64'))
 }
 
+function urlEncodeBase64(base64) {
+  return base64.replace(/=/g, '')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_');
+}
+
+
+function encodePart(obj) {
+  return base64UrlEncode(JSON.stringify(obj))
+}
+
+function jt(header, payload, secret) {
+  if(!header || !secret) return null
+  header = encodePart(header)
+  payload = encodePart(payload)
+  let jt_ = `${header}.${payload}`
+  const sig = urlEncodeBase64(crypto.createHmac('sha256', secret).update(jt_).digest('base64'))
+  return `${jt_}.${sig}`
+}
 module.exports = jt
--- test/test.js
+++ test/test.js
@@ -6,12 +6,20 @@ const jt = require('../')
 describe("jt", () => {
   describe("jt", () => {
 
-    it("should ignore nulls", () => {
-      assert.equal(jt(), null)
+    it("should ignore nulls", () => assert.equal(jt(), null))
+    it("should fail if doesn't have enough parameters", () => assert.equal(jt({},{}), null))
     })
 
-    it("should fail if doesn't have enough parameters", () => {
-      assert.equal(jt({},{}), null)
+    const header1 = {
+        iss: "authSvc",
+        sub: "userRoles",
+        exp: 1630730447160,
+    }
+    const payload1_empty = null
+    const secret1 = "my secret key"
+    const token1 = "eyJpc3MiOiJhdXRoU3ZjIiwic3ViIjoidXNlclJvbGVzIiwiZXhwIjoxNjMwNzMwNDQ3MTYwfQ.bnVsbA.KbkdXYDFesS0acvAu22t8wi2XASyyhLZUWDTMB9PmUw"
+
+    it("should return a valid token with empty payload", () => assert.equal(jt(header1,payload1_empty,secret1), token1))
     })
 
   })
